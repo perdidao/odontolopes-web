b4e6124653150987fd45c6465284a0a7040608ba
Lucas Almeida <lucasecalmeida@gmail.com>
2021-02-16 12:44:28 -0300

    Finalizando card de próximas consultas do dia

--- components/dashboard/Consultas.vue
+++ components/dashboard/Consultas.vue
@@ -8,7 +8,15 @@
         :key="item.id"
         class="dashboard-appointments__item"
       >
-        <p class="dashboard-appointments__name">{{ item.patient.name }}</p>
+        <div>
+          <p class="dashboard-appointments__name">{{ item.patient.name }}</p>
+          <p class="dashboard-appointments__procedures">
+            <span v-for="(procedure, p) in item.procedures" :key="procedure.id">
+              {{ procedure.title
+              }}{{ p &lt; item.procedures.length - 1 ? ", " : "" }}
+            </span>
+          </p>
+        </div>
         <p class="dashboard-appointments__date">
           {{ Moment(item.date).fromNow() }}
         </p>
